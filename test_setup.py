51904a47bd0515ff09b1b15d8980ca94907d3b74
Shruti T Sakpal <shrutitsakpal47@gmail.com>
2026-07-30 18:27:11 +0530

    Initialize project structure

---
+++ test_setup.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import sklearn
+import openpyxl
+import sqlalchemy
+import pymysql
+import mysql.connector
+
+print("✅ All libraries imported successfully!")
